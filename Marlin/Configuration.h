229e19b5dde6c96dbff30e335af29a98a90b5c27
Dan <dan@danpham.com>
2020-03-25 23:12:58 +0100

    Backing up bed PIDs correct gains

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -515,10 +515,10 @@
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
-
+  #define DEFAULT_bedKp 39.68
+  #define DEFAULT_bedKi 7.73
+  #define DEFAULT_bedKd 135.73
+  
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
   //#define DEFAULT_bedKp 97.1
